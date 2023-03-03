a5b8166c45c09a3891a29a77bce9031d22d84409
Fermin Jimenez <dill2897@gmail.com>
2023-03-02 18:25:16 -0600

    add

--- src/app/user/views/update/update.component.ts
+++ src/app/user/views/update/update.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.css']
 })
-export class UpdateComponent {
+export class UpdateComponent implements OnInit {
+
+  users : User[];
+  aux:any;
+  data:boolean;
+  errorMsg:any;
+  name:boolean;
+  grade:boolean;
+
+  constructor(private userService : UserService, private router : Router){}
+
+  ngOnInit():void{
+    this.getUsers()
+  }
+
+  getUsers(){
+
+    this.userService.getPosts().subscribe(
+      result => {
+
+        this.aux = result['no_data']
+        //console.log(this.aux)
+        if(this.aux == 'true'){
+          //console.log("No data")
+          this.data = true
+        }else{
+          //console.log(result)
+          this.users = result
+        }
+
+      }, (error) => {
+        console.log(error);
+        this.errorMsg = error;
+      }
+
+    )
+  }
+
+  modUsr = new FormGroup({
+    name: new FormControl( '', Validators.required),
+    grade : new FormControl( '', Validators.required)
+  })
+
+  modUser(){
+
+  }
 
 }
