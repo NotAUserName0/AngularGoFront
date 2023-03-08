25674b62b4fe7feb46221945fa2d6501f417cc84
Fermin Jimenez <dill2897@gmail.com>
2023-03-08 09:20:55 -0600

    final

--- src/app/user/views/show/show.component.ts
+++ src/app/user/views/show/show.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 
@@ -13,7 +14,6 @@ export class ShowComponent implements OnInit {
   users : User[];
   data:boolean;
   aux:any;
-  errorMsg:any;
 
   constructor(private userService : UserService){}
 
@@ -25,20 +25,26 @@ export class ShowComponent implements OnInit {
 
     this.userService.getPosts().subscribe(
       result => {
-
         this.aux = result['no_data']
-        //console.log(this.aux)
         if(this.aux == 'true'){
-          //console.log("No data")
           this.data = true
+          Swal.fire({
+            icon: 'question',
+            title: 'Oops...',
+            text: 'Nothing to see here!',
+          });
         }else{
-          //console.log(result)
           this.users = result
         }
 
       }, (error) => {
         console.log(error);
-        this.errorMsg = error;
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error,
+          confirmButtonColor: 'Red',
+        });
       }
 
     )
